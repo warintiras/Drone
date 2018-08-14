d3cfe6b97f4bd90d1662297ca8e56e7a776551aa
warintiras <warintiras@gmail.com>
2018-08-14 18:10:24 +0700

    Laravel From Scratch: Part 8-Edit and Delete Data, Part 9 - User Authentication

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -51,6 +51,7 @@ class PostsController extends Controller
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+        $post->user_id = $auth()->user()->id;
         $post->save();
 
         return redirect('/posts')->with('success', 'Post Created');
@@ -76,7 +77,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -88,7 +90,19 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        //Update Post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
@@ -99,6 +113,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post -> delete();
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,11 @@
  */
 
 Route::get('/', 'PagesController@index');
-
 Route::get('/about', 'PagesController@about');
-
 Route::get('/services', 'PagesController@services');
 
 Route::resource('posts', 'PostsController'); //Map routes to functions.
+
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index');
